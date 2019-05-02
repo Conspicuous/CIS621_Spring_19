02c9a325073ab894584849a15ed664aa68345760
Conspicuous <cpdorn93@gmail.com>
2019-05-02 19:47:43 -0400

    Final Submission
    This should work in all contexts!

--- HW_3/Controller.js
+++ HW_3/Controller.js
@@ -8,31 +8,28 @@ $(document).ready(function () {
 
 
     // Depending on the TYPE of browser, do different things. SEE PAGE 300. Coding HTML5 can be dangerous for error checking.
-    $("#contact").submit( //On Submit of the Page...
+   $("#contact").submit( //On Submit of the Page...
         function (evt) {
-            // alert("Submitting...");
-            //STEP ONE: Does the browser support HTML5 error checking? For this application, the usage is simple
-            //and therefore we don't need jquery methods here...
 
-            isValid = true; // By default, the form is valid on submission.
+
+            let isValid = true; // By default, the form is valid on submission. variable init is NOT redundant. Without it, you can carry this over to certain if/else cases.
 
             isValid = validateForm();
             // Error check
             if (isValid === false) {
                 evt.preventDefault(); //Don't submit
                 validateForm(); //recursive call to reposition the text and to display error messages
+
                 //$("#message").load("ThankYou.html #TestAA"); //AJAX Load Call
             }else{
-               //  alert("true");
-                $("#message").load("ThankYou.html #ThankYouBody"); //AJAX Load Call
+                $("#formfield").addClass("hidden"); // hide class
+                $("#ThankYouPage").removeClass("hidden"); //show thank you
+                setTimeout()
+                //$("#message").load("ThankYou.html #TestAA"); //AJAX Load Call
 
-
-               // window.location.href('http://localhost/ThankYou.html');
-            }
-
+            } //End Else
 
         } // End Function
-
 
     ) // End Submit
 
@@ -50,9 +47,13 @@ function validateForm() {
     //NAME
 
     var fullName = $("#name").val().trim();
+    var fullNamePattern=/^[0-9]+/;
     if (fullName == "") {
         $("#name").next().text("This field is required.");
         valid = false;
+    }else if(fullNamePattern.test(fullName)){
+        $("#name").next().text("Please use roman numerals ex: (I, II, III) \n for numbers in your name.")
+
     } else {
         $("#name").next().text("");
     }
@@ -65,11 +66,6 @@ function validateForm() {
     var email = $("#email").val().trim(); // trim whitespace characters
     // validate the email entry with a regular expression
     var emailPattern= /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-   // let parts = email.split("@"); //split email address at the @
-
-    // var address= "(^[\\w!#$%&'*+/=?^´{|}~-]+(\\.[\\w!#$%&'*+/=?^´{|}~-]+)*$)";
-    // var quotedText= "(^\"(([^\\\\\"])|
-    //
 
     if (email.length === 0) {
             $("#email").next().text("This field is required.");
@@ -85,27 +81,6 @@ function validateForm() {
         }
     $("#email").val(email);
 
-       // // let parts = email.split("@"); //split email address at the @
-       //  corr = true;
-       //  switch (parts) {
-       //      case parts.length !== 2:
-       //          corr = false;
-       //          break;
-       //      case parts[0].length>50:
-       //          corr = false;
-       //          break;
-       //      case parts[1].length>255:
-       //          corr = false;
-       //          break
-
-
-        //
-        // } //End Switch
-        // if (!(corr)){return false;} //if the result of the switch is false...return false
-
-// Keys: ()== substring, ^ = search for at start of string, extra slashes are used to escape special characters
-        // Literal translation... substring 1: match at the start of the sequence, a sequence of characters that may include any number of repetitions,
-        //and include the special characters ! # $ & % ' ´ =
 
 
 
@@ -113,17 +88,13 @@ function validateForm() {
 
 
 
-    // if (emailCorrect($("#email"))) { // if true
-    //     valid = true;
-    //     alert(valid + ": Email Valid")
-    // } else {
-    //     valid = false;
-    //     alert(valid + ": Email Invalid. Format Requirements: \n Maximum 50 characters before @ symbol, 1 @ symbol. ")
-    // }
+
+
+
 
     //PHONE
 
-    var phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+    var phonePattern = /^\d{3}-\d{3}-\d{4}/;
     var phone = $("#phone").val().trim();
     if (phone.length === 0) {
         $("#phone").next().text("This field is required. Format: 555-555-5555");
@@ -139,20 +110,20 @@ function validateForm() {
 
 
     //Country
-
-    var Country = /^\d{3}-\d{3}-\d{4}$/;
-    var phone = $("#phone").val().trim();
-    if (phone.length === 0) {
-        $("#phone").next().text("This field is required. Format: 555-555-5555");
+ //
+    var countryPattern = /^[A-Za-z]{3,6}/;
+    var country = $("#country").val().trim();
+    if (country.length === 0) {
+        $("#country").next().text("This field is required. Please use abbreviation.");
         valid = false;
-    } else if ( !phonePattern.test(phone) ) {
-        $("#phone").next().text("Use 555-555-5555 format.");
+    } else if ( !countryPattern.test(country) ) {
+        $("#country").next().text("Did you use only letters? Please use 3-6 letters.");
         valid = false;
     } else {
-        $("#phone").next().text("");
+        $("#country").next().text("");
         valid=true;
     }
-    $("#phone").val(phone);
+    $("#country").val(country);
 
     //error
 

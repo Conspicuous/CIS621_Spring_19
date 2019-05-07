27c4d6269021515bab14ebee362591725b343173
Conspicuous <cpdorn93@gmail.com>
2019-05-07 00:12:19 -0400

    Updated 5/7/19
    Still IP

---
+++ FinalProject_Scratch/Hard_Coded_MOS_Data_File.js
@@ -0,0 +1,214 @@
+/* HARD CODED VALUES. Created a class which on deployment will be used for handling front end ops.
+ *
+  * Initially...for 2 MOS Sites, 12Z Runs **ONLY**, *3* days (2*3)= 6 sets to fill.
+  *
+  * Data structure decision--
+  * Given future production rollout projected for this project inc database, need efficient large scale data struct
+  *
+  * */
+//Also using to demonstrate the jquery function getScript to load and execute an external script, which
+// can subsqeuntly be referenced. This allows us to retain modularity and separate code components for better readability.
+
+//Does the class perform read in of file?
+
+//MEX_MOS_Run_Data object instances are complete storage units for a single MOS-MEX run for a single location
+/*Construction...
+*
+* Map
+*
+*Each time the constructor is called, randomly generate a mos table.
+*
+*Store this table in json file
+*Try storing all at same time after.
+* */
+
+// alert("Hello 0");
+class MOS_Run_Data {
+
+    constructor(location, date) {
+        //let Values = GenerateFauxValues;
+        this.location = location; // 4 letter location identifer
+        this.date = date; // Date of run UTC
+
+
+    } // End Constructor
+
+
+} //End Class
+
+class MEX_MOS extends MOS_Run_Data {
+    h_temp = [];
+    l_temp = [];
+    POP24 = [];
+    windDir = [];
+    windSpeed = [];
+    QPF24 = [];
+
+    //Default no constructor--> super(args)
+    constructor(location, date, run_time = 12) {
+        super(location, date);
+        this.runTime = run_time;
+    }
+
+    show() {
+        alert(this.h_temp)
+    };
+
+    generateFauxValues() { // Create Randomized MOS data for every instance uniquely!
+        //alert("Hello");
+
+
+        for (let i = 1; i < 8; i++) { // for days tomorrow through 7..
+            this.POP24.push((i % 2 === 0) ? Math.round(Math.random() * (+100 - +60) + 60) : Math.round(Math.random() * (+35 - +15) + 15)); //ternary operator, even vs odd days.
+            this.windDir.push((i % 2 === 0) ? Math.floor(Math.random() * (+360 - +0)) : Math.floor(Math.random() * (+270 - +90) + 90));
+            this.h_temp.push(Math.round((Math.random() * (+79 - +62) + 62))); //high temp for day +1 (5/5 12z forecasts for 5/6 high and not 5/5)
+            this.l_temp.push(Math.round(Math.min(Math.random() * (+60 - +49) + 49, this.h_temp[i - 1]))); //the low temp is the lower of either a random number between 70 and 49 or the randomly generated high temperature for that day.
+            this.windSpeed.push(Math.round(Math.random() * (+20 - +1) + 1));
+            this.QPF24.push(Math.round(Math.random() * (+5 - +0)));
+        }
+
+        this.l_temp.push(49); //for 8th low temp (12z run), just make 49 to verify output.
+
+
+    }; //End MEX_MOS
+
+
+//error process
+
+
+    // End constructor
+
+    // GenerateFauxValues(){ //
+    //
+    //
+    //     let QPF24= {};
+    //
+    //     QPF24["Day 1"]={id:1, value:0};
+    //     QPF24["Day 2"]={id:2, value:2};
+    //     QPF24["Day 3"]={id:3, value:0};
+    //     QPF24["Day 4"]={id:4, value:5};
+    //     QPF24["Day 5"]={id:5, value:0};
+    //     QPF24["Day 6"]={id:6, value:1};
+    //     QPF24["Day 7"]={id:7, value:0};
+    //     this.QPF24 =QPF24;
+    //
+    //   for (let i = 1; i<7; i++){ // for days
+    //       POP24.push( (i%2===0) ? Math.round(Math.random() * (+100 - +60)+60):Math.round(Math.random()*(+35- +15)+15)); //ternary operator, even vs odd days.
+    //       windDir.push(i%2===0) ? Math.floor(Math.random()*(+360 - +0)):Math.floor(Math.random()*(+270- +90)+90);
+    //       h_temp_PHL.push(Math.floor((Math.random()*(+76 - +62)+ 62))); //high temp for day +1 (5/5 12z forecasts for 5/6 high and not 5/5)
+    //       l_temp_PHL.push(Math.floor(Math.max(Math.random()*(+70 - +49)+49,h_temp_PHL[i-1]))); //the low temp is the larger of either a random number between 70 and 49 or the randomly generated high temperature for that day.
+    //       windSpeed.push(Math.floor(Math.random()*(+20 - +1)+1));
+    //   }
+    //
+    //       this.POP24=POP24;
+    //
+    //   //for a 12z run, there are 8 lows and 7 highs
+    //   l_temp_PHL.push(Math.floor(Math.random()*(+70 - +49)+49));
+    //
+    //
+    //   return [POP24, windDir,h_temp_PHL,l_temp_PHL,windSpeed]; }
+
+
+} //End class
+
+
+//let S_h_temp_PHL = 63; //Degrees F, Starting High Temp PHL
+// let S_l_temp_PHL = 56;
+// let S_windSpeed = 10;
+//let S_windDir = 0;
+/*  The wind direction (WDIR) forecasts are coded as two digits, in units of tens of degrees measured clockwise from true north (01 = 10° to 36 = 360°); calm conditions are coded "00".
+*The two-digit wind speed (WSPD) forecasts are given to the nearest knot, with a maximum speed of 98 knots; calm conditions are coded "00".
+   * Missing WDIR and WSPD forecasts are coded as "99". */
+
+//let S_POP_24 = 80;
+
+//Generate random POP values...even days between 80 and 100% prob, odd days 15 to 35% prob
+//let Fake_MEX_MOS_Values=generateFauxValues(); //Generate Faux MOS Values!
+//let POP24 = [];
+//let h_temp_PHL =[];
+//let h_temp_DEN =[];
+//let l_temp_PHL = [];
+//let l_temp_DEN = [];
+//let windSpeed = [];
+//let windDir = [];
+
+
+// function generateFauxValues(){
+//
+//
+//     for (let i = 1; i<7; i++){ // for days
+//         POP24.push( (i%2===0) ? Math.round(Math.random() * (+100 - +60)+60):Math.round(Math.random()*(+35- +15)+15)); //ternary operator, even vs odd days.
+//         windDir.push(i%2===0) ? Math.floor(Math.random()*(+360 - +0)):Math.floor(Math.random()*(+270- +90)+90);
+//         h_temp_PHL.push(Math.floor((Math.random()*(+76 - +62)+ 62))); //high temp for day +1 (5/5 12z forecasts for 5/6 high and not 5/5)
+//         l_temp_PHL.push(Math.floor(Math.max(Math.random()*(+70 - +49)+49,h_temp_PHL[i-1]))); //the low temp is the larger of either a random number between 70 and 49 or the randomly generated high temperature for that day.
+//         windSpeed.push(Math.floor(Math.random()*(+20 - +1)+1));
+//     }
+//
+//     //for a 12z run, there are 8 lows and 7 highs
+//     l_temp_PHL.push(Math.floor(Math.random()*(+70 - +49)+49));
+//
+//
+//     return [POP24, windDir,h_temp_PHL,l_temp_PHL,windSpeed];
+//
+//
+//
+// } // End Function Generate Faux Values
+
+
+//For demo, the QPF values will remain constant across sites.
+// let QPF24= {};
+//
+// QPF24["Day 1"]={id:1, value:0};
+// QPF24["Day 2"]={id:2, value:2};
+// QPF24["Day 3"]={id:3, value:0};
+// QPF24["Day 4"]={id:4, value:5};
+// QPF24["Day 5"]={id:5, value:0};
+// QPF24["Day 6"]={id:6, value:1};
+// QPF24["Day 7"]={id:7, value:0};
+
+
+// let MOS_KPHL_5_5 = new MEX_MOS_Run_Data("KPHL",552019,h_temp_PHL,l_temp_PHL,windSpeed,windDir,POP24,QPF24);
+// let MOS_KPHL_5_5 = new MEX_MOS("KPHL",552019);
+//
+// let MOS_KPHL_5_4 = new MEX_MOS("KPHL",542019);
+// let MOS_KPHL_5_3 = new MEX_MOS("KPHL",532019);
+// let MOS_KPHL_5_2 = new MEX_MOS("KPHL",522019);
+// let MOS_KPHL_5_1 = new MEX_MOS("KPHL",512019);
+// let MOS_KPHL_4_30 = new MEX_MOS("KPHL",4302019);
+// let MOS_KPHL_4_29 = new MEX_MOS("KPHL",4292019);
+
+
+//todo: Iterate through MOS
+//********
+//let Dummy_MEX_Instances = [MOS_KPHL_5_5,MOS_KPHL_5_4,MOS_KPHL_5_3, MOS_KPHL_5_2,MOS_KPHL_5_1,MOS_KPHL_4_30,MOS_KPHL_4_29];
+// MOS_KPHL_4_29.generateFauxValues();
+// MOS_KPHL_4_29.show();
+//********
+
+
+// let MOS_KDEN_5_5 = new MEX_MOS();
+// let MOS_KDEN_5_4 = new MEX_MOS();
+// let MOS_KDEN_5_3 = new MEX_MOS();
+// let MOS_KDEN_5_2 = new MEX_MOS();
+// let MOS_KDEN_5_1 = new MEX_MOS();
+// let MOS_KDEN_4_30 = new MEX_MOS();
+// let MOS_KDEN_4_29 = new MEX_MOS();
+
+
+// let MOS_KWWD_5_5 = new MEX_MOS();
+// let MOS_KWWD_5_4 = new MEX_MOS();
+// let MOS_KWWD_5_3 = new MEX_MOS();
+// let MOS_KWWD_5_2 = new MEX_MOS();
+// let MOS_KWWD_5_1 = new MEX_MOS();
+// let MOS_KWWD_4_30 = new MEX_MOS();
+// let MOS_KWWD_4_29 = new MEX_MOS();
+
+//todo: local storage JSON file...
+
+// try {
+//     localStorage["MOSHardCode"]=JSON.stringify(MOS_KPHL_4_29);
+//     alert("trying");
+//
+// } catch (e) {
+//     alert("catching");
+// }
--- FinalProject_Scratch/Main_Controller.js
+++ FinalProject_Scratch/Main_Controller.js
@@ -1,40 +1,125 @@
-$(function () {
+"use strict";
+
+$.getScript("Hard_Coded_MOS_Data_File.js").then(() => { //promise return (on success), arrow func syntax
+    // alert("Hello 27");
+    $(function () {
+
+        //todo: local storage
+        //todo: read json: https://stackoverflow.com/questions/17724017/using-jquery-to-build-table-rows-from-ajax-responsejson
+        //todo: more than 1 date (one run)
+
+        //todo: compare runs
+
+        //Temporary Hard code (get to work first...)
+        let MOS_MOS = new MEX_MOS("KPHL", 552019);
+        MOS_MOS.generateFauxValues();
 
 
-    var Sky_Conds = ["CL", "PC", "OC"];
-    var dummy_Conditions = {
-        high_Tmp: 72,
-        low_Temp: 68,
-        wnd_Spd: 15,
-        wind_Dir: 270,
-        sky_Cdtns: "Scattered",
-        pop12: 15,
-        qpf12: 0
-    };
+        let dummy_Conditions = { //temp
+            high_Tmp: MOS_MOS.h_temp,
+            low_Temp: MOS_MOS.l_temp,
+            wnd_Spd: MOS_MOS.windSpeed,
+            wind_Dir: MOS_MOS.windDir,
 
-    $("#high_temp").text(dummy_Conditions.high_Tmp);
-    $("#low_temp").text(dummy_Conditions.low_Temp);
-    $("#wnd_speed").text(dummy_Conditions.wnd_Spd);
-    $("#wnd_dir").text(dummy_Conditions.wind_Dir);
-    $("#cld_cvr").text(dummy_Conditions.sky_Cdtns);
-    $("#POP12").text(dummy_Conditions.pop12);
-    $("#QPF12").text(dummy_Conditions.qpf12);
+            pop24: MOS_MOS.POP24,
+            qpf24: MOS_MOS.QPF24
+        };
 
 
-    $.ajax({
-        url: "SiteListMOS.txt",
-        dataType: "text",
-        success: function(data) {
-            var autoCompleteData = data.split('\n');
-            $("#SitePicker").autocomplete({
-                source: function(request, response) {
-                    var results = $.ui.autocomplete.filter(autoCompleteData, request.term);
-                    response(results.slice(0, 5)); // Display the first 5 results
-                    $("#SitePicker").value(response); //Plug the value for the response
-                }
+        // var dummy_conditions_Older = {high_Tmp: 70,
+        //     low_Temp: 66,
+        //     wnd_Spd: 12,
+        //     wind_Dir: 250,
+        //     sky_Cdtns: "Scattered",
+        //     pop12: 2,
+        //     qpf12: 0}
+        // ;
+        //
+        // var dummy_Conditions_older_older = {
+        //     high_Tmp: [70, 66, 73, 59, 66, 76, 72]};
+
+
+        // $("#high_temp").text(dummy_Conditions.high_Tmp[0]); //CHANGE to row i= 1 to 6
+        // $("#low_temp").text(dummy_Conditions.low_Temp[0]);
+        // $("#wnd_speed").text(dummy_Conditions.wnd_Spd);
+        // $("#wnd_dir").text(dummy_Conditions.wind_Dir);
+        // $("#cld_cvr").text(dummy_Conditions.sky_Cdtns);
+        // $("#POP12").text(dummy_Conditions.pop12);
+        // $("#QPF12").text(dummy_Conditions.qpf12);
+
+// AUTOCOMPLETE AJAX Call
+        $.ajax({
+            url: "SiteListMOS.txt",
+            dataType: "text",
+            success: function (data) {
+                var autoCompleteData = data.split('\n');
+                $("#SitePicker").autocomplete({
+                    source: function (request, response) {
+                        var results = $.ui.autocomplete.filter(autoCompleteData, request.term);
+                        response(results.slice(0, 5)); // Display the first 5 results
+                        $("#SitePicker").value(response); //Plug the value for the response
+
+                    },
+                    appendTo: $("#Site_Picker_Form")
+
+
+                });
+            }
+        });
+
+
+        $("#accordion").accordion({ //ACCORDION
+            heightStyle: "content",
+            collapsible: true, //allow the collapse of all panels (not default functionality)
+            active: false // start collapsed
+
+        }); // END Accordion
+
+
+
+        var dummyDates = ["5/5/2019", "5/6/2019", "5/7/2019", "5/8/2019", "5/9/2019", "5/10/2019", "5/11/2019", "5/12/2019"];
+        var data = [dummyDates, //headers
+            dummy_Conditions.high_Tmp, dummy_Conditions.low_Temp, dummy_Conditions.wnd_Spd, dummy_Conditions.wind_Dir, dummy_Conditions.pop24, dummy_Conditions.qpf24
+        ];
+
+        makeTable($("#Current_Run"), data);
+
+        function makeTable(container, data) {
+            var table = $("<table/>").addClass('topazCells');
+            $.each(data, function (rowIndex, r) {
+                var row = $("<tr/>");
+                $.each(r, function (colIndex, c) {
+                    row.append($("<t" + (rowIndex == 0 ? "h" : "d") + "/>").text(c));
+                });
+                table.append(row);
             });
+            return container.append(table);
         }
-    });
 
 
-});
+
+        $("#Early_Date").datepicker({
+            minDate: -7, // The minimum date to pick from is 7 days in the past (1 week)
+            maxDate: -1 //The maximum date to pick from is yesterday
+        });
+
+    });// end ready
+}); //end get Script
+
+
+
+//todo: button to select Date--> gets the MOS run for that date, at bottom of page, visible if user is in correct part of accordion (use class)
+// todo: Put in a calendar drop down menu
+
+
+//todo: Current run: date insert
+
+/*
+*  function --> take in current date, and current local time
+*  ---> May 1st 930pm
+*  April 29th, 12Z, April 29th, 00Z. April 28th, 12Z. ...... .....
+* return the 14 most recent runs based on the current date and time.
+*
+*
+*
+* */
